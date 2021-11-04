2b66817544e39a3d9d762a4084900b13ad0919d0
Philipp Gaissert <gaissert@seas.upenn.edu>
2021-11-04 01:24:55 -0400

    Indentation formatting

--- src/Fraction.java
+++ src/Fraction.java
@@ -3,65 +3,65 @@
  */
 public class Fraction {
 
-	private int numerator;
-	private int denominator;
+    private int numerator;
+    private int denominator;
 
-	/**
-	 * Constructs a Fraction object
-	 * 
-	 * @param a - numerator
-	 * @param b - denominator
-	 */
-	public Fraction(int a, int b) {
-		// TODO
+    /**
+     * Constructs a Fraction object
+     * 
+     * @param a - numerator
+     * @param b - denominator
+     */
+    public Fraction(int a, int b) {
+        // TODO
 
-		/* REMEMBER TO MODIFY THIS TO THROW THE EXCEPTION */
-	}
+        /* REMEMBER TO MODIFY THIS TO THROW THE EXCEPTION */
+    }
 
-	/**
-	 * Getter for the fraction's numerator
-	 * 
-	 * @return numerator
-	 */
-	public int getNumerator() {
-		// TODO
-		return 0;
-	}
+    /**
+     * Getter for the fraction's numerator
+     * 
+     * @return numerator
+     */
+    public int getNumerator() {
+        // TODO
+        return 0;
+    }
 
-	/**
-	 * Getter for the fraction's denominator
-	 * 
-	 * @return denominator
-	 */
-	public int getDenominator() {
-		// TODO
-		return 0;
-	}
+    /**
+     * Getter for the fraction's denominator
+     * 
+     * @return denominator
+     */
+    public int getDenominator() {
+        // TODO
+        return 0;
+    }
 
-	/**
-	 * Adds the given fraction to this fraction
-	 * 
-	 * @param other - another fraction
-	 */
-	void add(Fraction other) {
-		// TODO
-	}
+    /**
+     * Adds the given fraction to this fraction
+     * 
+     * @param other - another fraction
+     */
+    void add(Fraction other) {
+        // TODO
+    }
 
-	/**
-	 * Reduces this fraction to its lowest terms
-	 */
-	public void reduceToLowestTerms() {
-		// TODO
-	}
-	
-	/**
-	 * Returns the fraction's value in decimal form
-	 * 
-	 * @return fraction in decimal form
-	 */
-	public double toDecimal() {
-		// TODO
-		return 0.0;
-	}
+    /**
+     * Reduces this fraction to its lowest terms
+     */
+    public void reduceToLowestTerms() {
+        // TODO
+    }
+
+    /**
+     * Returns the fraction's value in decimal form
+     * 
+     * @return fraction in decimal form
+     */
+    public double toDecimal() {
+        // TODO
+        return 0.0;
+    }
 
 }
--- src/NotPositiveException.java
+++ src/NotPositiveException.java
@@ -1,8 +1,8 @@
 
 public class NotPositiveException extends Exception {
-	NotPositiveException(String message) {
-		// appealing to the super class
-		// use Exception constructor
-		super(message);
-	}
+    NotPositiveException(String message) {
+        // appealing to the super class
+        // use Exception constructor
+        super(message);
+    }
 }
